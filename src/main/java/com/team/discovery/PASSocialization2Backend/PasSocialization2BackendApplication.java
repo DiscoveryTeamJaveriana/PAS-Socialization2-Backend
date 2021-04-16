0570b9fcc95def84e6525a8a844fc01d6d00d2f7
Diego Borrero <diego.borrero@payulatam.com>
2021-04-16 12:09:59 -0500

    First Commit

---
+++ src/main/java/com/team/discovery/PASSocialization2Backend/PasSocialization2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.team.discovery.PASSocialization2Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasSocialization2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasSocialization2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/discovery/PASSocialization2Backend/PasSocialization2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.discovery.PASSocialization2Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasSocialization2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
