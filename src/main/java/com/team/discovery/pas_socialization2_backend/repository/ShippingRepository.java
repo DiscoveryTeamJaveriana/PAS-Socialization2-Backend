0456a6e9f93f11bfeb75938a460331dfd7b0aa9b
DxMortem <die.9511@gmail.com>
2021-04-17 20:11:45 -0500

    Implementing first phase cron processor

--- src/main/java/com/team/discovery/pas_socialization2_backend/repository/ShippingRepository.java
+++ src/main/java/com/team/discovery/pas_socialization2_backend/repository/ShippingRepository.java
@@ -1,7 +1,13 @@
 package com.team.discovery.pas_socialization2_backend.repository;
 
+import java.util.List;
+
 import com.team.discovery.pas_socialization2_backend.model.despachos_db.Shipping;
+import com.team.discovery.pas_socialization2_backend.model.despachos_db.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShippingRepository extends JpaRepository<Shipping, Long> {
+
+    List<Shipping> findShippingsByState(final State state);
+
 }
--- src/main/java/com/team/discovery/pas_socialization2_backend/service/scheduled/ScheduledPendingShippingQuery.java
+++ src/main/java/com/team/discovery/pas_socialization2_backend/service/scheduled/ScheduledPendingShippingQuery.java
@@ -1,7 +1,11 @@
 package com.team.discovery.pas_socialization2_backend.service.scheduled;
 
+import java.util.List;
 import javax.transaction.Transactional;
 
+import com.team.discovery.pas_socialization2_backend.model.despachos_db.Shipping;
+import com.team.discovery.pas_socialization2_backend.model.despachos_db.State;
+import com.team.discovery.pas_socialization2_backend.service.IShippingService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,11 +18,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledPendingShippingQuery {
 
+    private IShippingService shippingService;
+
     @Transactional
     @Scheduled(cron = "${cron.pending_shipping}")
     public void getPendingShipping(){
-        //log.info("Starting query");
-        //log.info("Finishing query");
+        log.info("Starting cron pending_shipping");
+        List<Shipping> shippingList = shippingService.getShippingByStatus(State.DESPACHO_CARGADO);
+        if (!shippingList.isEmpty() && shippingList.size()>0) {
+            shippingService.processPendingShipping(shippingList);
+        }
+        log.info("Finishing cron pending_shipping");
     }
 
 }
