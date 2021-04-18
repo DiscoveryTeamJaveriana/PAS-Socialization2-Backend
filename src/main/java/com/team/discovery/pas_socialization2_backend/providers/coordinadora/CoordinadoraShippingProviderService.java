87b95efc7796f255a185ab699c558de49060a5c4
DxMortem <die.9511@gmail.com>
2021-04-18 02:33:19 -0500

    Implementing coordinadora adapter + mock

--- src/main/java/com/team/discovery/pas_socialization2_backend/providers/coordinadora/CoordinadoraShippingProviderService.java
+++ src/main/java/com/team/discovery/pas_socialization2_backend/providers/coordinadora/CoordinadoraShippingProviderService.java
@@ -1,13 +1,45 @@
 package com.team.discovery.pas_socialization2_backend.providers.coordinadora;
 
+import java.rmi.RemoteException;
+
 import com.team.discovery.pas_socialization2_backend.model.despachos_db.Shipping;
+import com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingInput;
+import com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingOutput;
+import com.team.discovery.pas_socialization2_backend.providers.coordinadora.service.Shipping_ws;
+import com.team.discovery.pas_socialization2_backend.providers.coordinadora.service.Shipping_wsStub;
 import com.team.discovery.pas_socialization2_backend.service.IShippingProvider;
+import org.apache.axis2.AxisFault;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CoordinadoraShippingProviderService implements IShippingProvider {
+
+    Shipping_ws shippingWs;
+
     @Override
     public Integer getOffer(Shipping shipping) {
-        return 10;
+        try {
+            buildConnection();
+        } catch (AxisFault axisFault) {
+            axisFault.printStackTrace();
+        }
+
+        ShippingInput shippingInput = new ShippingInput();
+        shippingInput.setAddress(shipping.getUser().getAddress());
+        shippingInput.setBoxesAmount(shipping.getBoxesAmount());
+        shippingInput.setTotalWeight(shippingInput.getTotalWeight());
+
+        try {
+            ShippingOutput shippingOutput = shippingWs.calculate(shippingInput);
+            return shippingOutput.getValue();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void buildConnection() throws AxisFault {
+        shippingWs = new Shipping_wsStub();
     }
 }
---
+++ src/main/java/com/team/discovery/pas_socialization2_backend/providers/coordinadora/service/Shipping_wsStub.java
@@ -0,0 +1,308 @@
+/**
+ * Shipping_wsStub.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.9  Built on : Nov 16, 2018 (12:05:37 GMT)
+ */
+package com.team.discovery.pas_socialization2_backend.providers.coordinadora.service;
+
+
+/*
+ *  Shipping_wsStub java implementation
+ */
+public class Shipping_wsStub extends org.apache.axis2.client.Stub
+    implements Shipping_ws {
+    private static int counter = 0;
+    protected org.apache.axis2.description.AxisOperation[] _operations;
+
+    //hashmaps to keep the fault mapping
+    private java.util.HashMap faultExceptionNameMap = new java.util.HashMap();
+    private java.util.HashMap faultExceptionClassNameMap = new java.util.HashMap();
+    private java.util.HashMap faultMessageMap = new java.util.HashMap();
+    private javax.xml.namespace.QName[] opNameArray = null;
+
+    /**
+     *Constructor that takes in a configContext
+     */
+    public Shipping_wsStub(
+        org.apache.axis2.context.ConfigurationContext configurationContext,
+        java.lang.String targetEndpoint) throws org.apache.axis2.AxisFault {
+        this(configurationContext, targetEndpoint, false);
+    }
+
+    /**
+     * Constructor that takes in a configContext  and useseperate listner
+     */
+    public Shipping_wsStub(
+        org.apache.axis2.context.ConfigurationContext configurationContext,
+        java.lang.String targetEndpoint, boolean useSeparateListener)
+        throws org.apache.axis2.AxisFault {
+        //To populate AxisService
+        populateAxisService();
+        populateFaults();
+
+        _serviceClient = new org.apache.axis2.client.ServiceClient(configurationContext,
+                _service);
+
+        _serviceClient.getOptions()
+                      .setTo(new org.apache.axis2.addressing.EndpointReference(
+                targetEndpoint));
+        _serviceClient.getOptions().setUseSeparateListener(useSeparateListener);
+    }
+
+    /**
+     * Default Constructor
+     */
+    public Shipping_wsStub(
+        org.apache.axis2.context.ConfigurationContext configurationContext)
+        throws org.apache.axis2.AxisFault {
+        this(configurationContext, "http://localhost:10019/shipping");
+    }
+
+    /**
+     * Default Constructor
+     */
+    public Shipping_wsStub() throws org.apache.axis2.AxisFault {
+        this("http://localhost:10019/shipping");
+    }
+
+    /**
+     * Constructor taking the target endpoint
+     */
+    public Shipping_wsStub(java.lang.String targetEndpoint)
+        throws org.apache.axis2.AxisFault {
+        this(null, targetEndpoint);
+    }
+
+    private static synchronized java.lang.String getUniqueSuffix() {
+        // reset the counter if it is greater than 99999
+        if (counter > 99999) {
+            counter = 0;
+        }
+
+        counter = counter + 1;
+
+        return java.lang.Long.toString(java.lang.System.currentTimeMillis()) +
+        "_" + counter;
+    }
+
+    private void populateAxisService() throws org.apache.axis2.AxisFault {
+        //creating the Service with a unique name
+        _service = new org.apache.axis2.description.AxisService("Shipping_ws" +
+                getUniqueSuffix());
+        addAnonymousOperations();
+
+        //creating the operations
+        org.apache.axis2.description.AxisOperation __operation;
+
+        _operations = new org.apache.axis2.description.AxisOperation[1];
+
+        __operation = new org.apache.axis2.description.OutInAxisOperation();
+
+        __operation.setName(new javax.xml.namespace.QName(
+                "http://www.example.com/wsdl/shipping.wsdl", "calculate"));
+        _service.addOperation(__operation);
+
+        _operations[0] = __operation;
+    }
+
+    //populates the faults
+    private void populateFaults() {
+    }
+
+    /**
+     * Auto generated method signature
+     *
+     * @see com.team.discovery.pas_socialization2_backend.providers.coordinadora.service.Shipping_ws#calculate
+     * @param shippingInput0
+     */
+    public com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingOutput calculate(
+            com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingInput shippingInput0)
+        throws java.rmi.RemoteException {
+        org.apache.axis2.context.MessageContext _messageContext = new org.apache.axis2.context.MessageContext();
+
+        try {
+            org.apache.axis2.client.OperationClient _operationClient = _serviceClient.createClient(_operations[0].getName());
+            _operationClient.getOptions().setAction("ns:calculate");
+            _operationClient.getOptions().setExceptionToBeThrownOnSOAPFault(true);
+
+            addPropertyToOperationClient(_operationClient,
+                org.apache.axis2.description.WSDL2Constants.ATTR_WHTTP_QUERY_PARAMETER_SEPARATOR,
+                "&");
+
+            // create SOAP envelope with that payload
+            org.apache.axiom.soap.SOAPEnvelope env = null;
+
+            env = toEnvelope(getFactory(_operationClient.getOptions()
+                                                        .getSoapVersionURI()),
+                    shippingInput0,
+                    optimizeContent(
+                        new javax.xml.namespace.QName(
+                            "http://www.example.com/wsdl/shipping.wsdl",
+                            "calculate")),
+                    new javax.xml.namespace.QName(
+                        "http://www.example.com/wsdl/shipping.wsdl",
+                        "shippingInput"));
+
+            //adding SOAP soap_headers
+            _serviceClient.addHeadersToEnvelope(env);
+            // set the message context with that soap envelope
+            _messageContext.setEnvelope(env);
+
+            // add the message contxt to the operation client
+            _operationClient.addMessageContext(_messageContext);
+
+            //execute the operation client
+            _operationClient.execute(true);
+
+            org.apache.axis2.context.MessageContext _returnMessageContext = _operationClient.getMessageContext(org.apache.axis2.wsdl.WSDLConstants.MESSAGE_LABEL_IN_VALUE);
+            org.apache.axiom.soap.SOAPEnvelope _returnEnv = _returnMessageContext.getEnvelope();
+
+            java.lang.Object object = fromOM(_returnEnv.getBody()
+                                                       .getFirstElement(),
+                    com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingOutput.class);
+
+            return (com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingOutput) object;
+        } catch (org.apache.axis2.AxisFault f) {
+            org.apache.axiom.om.OMElement faultElt = f.getDetail();
+
+            if (faultElt != null) {
+                if (faultExceptionNameMap.containsKey(
+                            new org.apache.axis2.client.FaultMapKey(
+                                faultElt.getQName(), "calculate"))) {
+                    //make the fault by reflection
+                    try {
+                        java.lang.String exceptionClassName = (java.lang.String) faultExceptionClassNameMap.get(new org.apache.axis2.client.FaultMapKey(
+                                    faultElt.getQName(), "calculate"));
+                        java.lang.Class exceptionClass = java.lang.Class.forName(exceptionClassName);
+                        java.lang.reflect.Constructor constructor = exceptionClass.getConstructor(java.lang.String.class);
+                        java.lang.Exception ex = (java.lang.Exception) constructor.newInstance(f.getMessage());
+
+                        //message class
+                        java.lang.String messageClassName = (java.lang.String) faultMessageMap.get(new org.apache.axis2.client.FaultMapKey(
+                                    faultElt.getQName(), "calculate"));
+                        java.lang.Class messageClass = java.lang.Class.forName(messageClassName);
+                        java.lang.Object messageObject = fromOM(faultElt,
+                                messageClass);
+                        java.lang.reflect.Method m = exceptionClass.getMethod("setFaultMessage",
+                                new java.lang.Class[] { messageClass });
+                        m.invoke(ex, new java.lang.Object[] { messageObject });
+
+                        throw new java.rmi.RemoteException(ex.getMessage(), ex);
+                    } catch (java.lang.ClassCastException e) {
+                        // we cannot intantiate the class - throw the original Axis fault
+                        throw f;
+                    } catch (java.lang.ClassNotFoundException e) {
+                        // we cannot intantiate the class - throw the original Axis fault
+                        throw f;
+                    } catch (java.lang.NoSuchMethodException e) {
+                        // we cannot intantiate the class - throw the original Axis fault
+                        throw f;
+                    } catch (java.lang.reflect.InvocationTargetException e) {
+                        // we cannot intantiate the class - throw the original Axis fault
+                        throw f;
+                    } catch (java.lang.IllegalAccessException e) {
+                        // we cannot intantiate the class - throw the original Axis fault
+                        throw f;
+                    } catch (java.lang.InstantiationException e) {
+                        // we cannot intantiate the class - throw the original Axis fault
+                        throw f;
+                    }
+                } else {
+                    throw f;
+                }
+            } else {
+                throw f;
+            }
+        } finally {
+            if (_messageContext.getTransportOut() != null) {
+                _messageContext.getTransportOut().getSender()
+                               .cleanup(_messageContext);
+            }
+        }
+    }
+
+    private boolean optimizeContent(javax.xml.namespace.QName opName) {
+        if (opNameArray == null) {
+            return false;
+        }
+
+        for (int i = 0; i < opNameArray.length; i++) {
+            if (opName.equals(opNameArray[i])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //http://localhost:10019/shipping
+    private org.apache.axiom.om.OMElement toOM(
+            com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingInput param,
+        boolean optimizeContent) throws org.apache.axis2.AxisFault {
+        try {
+            return param.getOMElement(com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingInput.MY_QNAME,
+                org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+        } catch (org.apache.axis2.databinding.ADBException e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    private org.apache.axiom.om.OMElement toOM(
+            com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingOutput param,
+        boolean optimizeContent) throws org.apache.axis2.AxisFault {
+        try {
+            return param.getOMElement(com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingOutput.MY_QNAME,
+                org.apache.axiom.om.OMAbstractFactory.getOMFactory());
+        } catch (org.apache.axis2.databinding.ADBException e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    private org.apache.axiom.soap.SOAPEnvelope toEnvelope(
+        org.apache.axiom.soap.SOAPFactory factory,
+        com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingInput param,
+        boolean optimizeContent, javax.xml.namespace.QName elementQName)
+        throws org.apache.axis2.AxisFault {
+        try {
+            org.apache.axiom.soap.SOAPEnvelope emptyEnvelope = factory.getDefaultEnvelope();
+            emptyEnvelope.getBody()
+                         .addChild(param.getOMElement(
+                             com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingInput.MY_QNAME,
+                    factory));
+
+            return emptyEnvelope;
+        } catch (org.apache.axis2.databinding.ADBException e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+    }
+
+    /* methods to provide back word compatibility */
+
+    /**
+     *  get the default envelope
+     */
+    private org.apache.axiom.soap.SOAPEnvelope toEnvelope(
+        org.apache.axiom.soap.SOAPFactory factory) {
+        return factory.getDefaultEnvelope();
+    }
+
+    private java.lang.Object fromOM(org.apache.axiom.om.OMElement param,
+        java.lang.Class type) throws org.apache.axis2.AxisFault {
+        try {
+            if (com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingInput.class.equals(
+                        type)) {
+                return com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingInput.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+            }
+
+            if (com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingOutput.class.equals(
+                        type)) {
+                return com.team.discovery.pas_socialization2_backend.providers.coordinadora.model.ShippingOutput.Factory.parse(param.getXMLStreamReaderWithoutCaching());
+            }
+        } catch (java.lang.Exception e) {
+            throw org.apache.axis2.AxisFault.makeFault(e);
+        }
+
+        return null;
+    }
+}
